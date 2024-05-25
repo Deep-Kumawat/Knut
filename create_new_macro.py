d0d21dd6fa6a314e72f330ebcedb62a12d591b60
deep-kumawat <deepdkmail@gmail.com>
2024-05-25 22:06:51 +0530

    1.Deleted temp code runner file 2. Divided the file into two separate files.     1. macro_maker.py - used to run already created macros     2. create_new_macro.py - used to create new macro 3. Added support for terminal commands 4. Modified readme

--- create_new_macro.py
+++ create_new_macro.py
@@ -13,9 +13,9 @@ listOfActions = []
 # ------------------------------------------------------------------------------
 print("Welcome to Knut. Here, you can map a macro to click a button(and more!)")
 # choices to make new macro or make a new one.
-# userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice: \n0. Exit\n1. Use your created shortcuts\n2. Create new macros\nYour Input ---> "))
+# userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice: \n0. Exit\n1. Use your created shortcuts\n2. Create new macros\n --> "))
 def createNewMacro():
-    userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice: \n0. Exit\n1. Create new macros\nYour Input ---> "))
+    userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice: \n0. Exit\n1. Create new macros\n--> "))
     while True:
         if userChoiceToMakeNewMacrosOrUseExisting == 0:  # 0 to exit
             return -1
@@ -26,74 +26,78 @@ def createNewMacro():
             # print(inputMacroKey)
             inputMacroKey = str(inputMacroKey[0])
             inputMacroKey = inputMacroKey[14:-6]
-            print("Your selected macro key was --->", inputMacroKey)
-            pickle.dump(inputMacroKey, open("inputMacroKeyStoringFile.dat", "wb"))
-            while True:  # asking the user what action he/she wants to execute. 0 to exit
-                newMacroActionType_UserInput = int(input(
-                    "Enter the number corresponding to the choice: \n0. Go Back\n1. Mouse Single Click\n2. Mouse Double Click\n3. Mouse Drag\n4. Key Press\n5. Text\n6. Key Combination\n7. Terminal Command\nYour Input ---> "))
-                if newMacroActionType_UserInput == 0:
-                    userChoiceToMakeNewMacrosOrUseExisting = int(input(
-                        "\nYou got back\n\nEnter the number corresponding to the choice: \n0. Exit\n1. Use your created shortcuts\n2. Create new macros\nYour Input ---> "))
-                    break
-                while newMacroActionType_UserInput != 0:
-                    if newMacroActionType_UserInput == 1:  # choice for single click
-                        input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the desired location:- "))
-                        for i in range(1, input_delay+1):
-                            print("tik tik", i)
-                            time.sleep(1)
-                        listForMouseSingleClick = ["MouseSingleClick", pyautogui.position().x, pyautogui.position().y]
-                        listOfActions.append(listForMouseSingleClick)
-                        break
-                    elif newMacroActionType_UserInput == 2:# Choice For Double Click
-                        #here goes the code for double click
-                        input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the desired location:- "))
-                        for i in range(1, input_delay+1):
-                            print("tik tik", i)
-                            time.sleep(1)
-                        listForMouseDoubleClick = ["MouseDoubleClick", pyautogui.position().x, pyautogui.position().y]
-                        listOfActions.append(listForMouseDoubleClick)
-                        break
-                    elif newMacroActionType_UserInput == 3:#for mouse drag
-                        input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the START position of the mouse drag:- "))
-                        for i in range(1, input_delay+1):
-                            print("tik tik", i)
-                            time.sleep(1)
-                        XstartPositionForDrag = pyautogui.position().x
-                        YstartPositionForDrag = pyautogui.position().y
-                        input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the END position of the mouse drag:- "))
-                        XendPositionForDrag = pyautogui.position().x
-                        YendPositionForDrag = pyautogui.position().y
-                        for i in range(1, input_delay+1):
-                            print("tik tik", i)
-                            time.sleep(1)
-                        listForMouseDrag = ["MouseDrag", [XstartPositionForDrag, YstartPositionForDrag], [XendPositionForDrag, YendPositionForDrag]]
-                        listOfActions.append(listForMouseDrag)
-                        break
-                    elif newMacroActionType_UserInput == 4:#For Keypress
-                        print("Press the key you want to be pressed with the macro(press escape after pressing the key)")
-                        time.sleep(1)
-                        actionKey = keyboard.record(until='esc')#its the key the user wants the program to press after he presses the macro key
-                        actionKey = str(actionKey[0])
-                        print(actionKey)
-                        actionKey = actionKey[14:-6]
-                        print("The key you pressed was --->", actionKey)
-                        listForKeypresses = ["Keypress", actionKey]
-                        listOfActions.append(listForKeypresses)
-                    elif newMacroActionType_UserInput == 5:#for text
-                        actionText = input("Enter the text you want to be typed with the macro(press escape after pressing the key)\nYour Input --->")
-                        listForText = ["Text", actionText]
-                        listOfActions.append(listForText)
-                    elif newMacroActionType_UserInput == 6:#for Key Combination
-                        actionKeyCombination = input("Enter the key combination/shortcut you want the macro to perform(Eg: ctrl,shift,alt,v)")
-                        actionKeyCombination = actionKeyCombination.split(',')
-                        listForKeyCombination = ["KeyCombination", actionKeyCombination]
-                        listOfActions.append(listForKeyCombination)                    
-                    elif newMacroActionType_UserInput == 7:#for terminal commands
-                        terminalCommand = input("Enter terminal command that you want to execute: ")
-                        listForKeyCombination = ["TerminalCommand", terminalCommand]
-                        listOfActions.append(listForKeyCombination)
-                        pickle.dump(listOfActions, open("listOfActionStoringFile.dat", "wb"))
-                        break
-                    else:#for an invalid input
-                        print("\nInvalid input try again!\n")
-                        userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice: \n0. Exit\n1. Create new macros\nYour Input ---> "))
+            print("Your selected macro key was --> ", inputMacroKey)
+            pickle.dump(inputMacroKey, open("inputMacroKey.dat", "wb"))
+            newMacroActionType_UserInput = int(input(
+                "Enter the number corresponding to the choice: \n0. Go Back\n1. Mouse Single Click\n2. Mouse Double Click\n3. Mouse Drag\n4. Key Press\n5. Text\n6. Key Combination\n7. Terminal Command\n--> "))
+            if newMacroActionType_UserInput == 0:
+                userChoiceToMakeNewMacrosOrUseExisting = int(input(
+                    "\nYou got back\n\nEnter the number corresponding to the choice: \n0. Exit\n1. Create new macros\n2. Use your created shortcuts\n--> "))
+                continue
+            if newMacroActionType_UserInput == 1:  # choice for single click
+                input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the desired location:- "))
+                for i in range(1, input_delay+1):
+                    print("tik tik", i)
+                    time.sleep(1)
+                listForMouseSingleClick = ["MouseSingleClick", pyautogui.position().x, pyautogui.position().y]
+                listOfActions.append(listForMouseSingleClick)
+                continue
+            elif newMacroActionType_UserInput == 2:# Choice For Double Click
+                #here goes the code for double click
+                input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the desired location:- "))
+                for i in range(1, input_delay+1):
+                    print("tik tik", i)
+                    time.sleep(1)
+                listForMouseDoubleClick = ["MouseDoubleClick", pyautogui.position().x, pyautogui.position().y]
+                listOfActions.append(listForMouseDoubleClick)
+                continue
+            elif newMacroActionType_UserInput == 3:#for mouse drag
+                input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the START position of the mouse drag:- "))
+                for i in range(1, input_delay+1):
+                    print("tik tik", i)
+                    time.sleep(1)
+                XstartPositionForDrag = pyautogui.position().x
+                YstartPositionForDrag = pyautogui.position().y
+                input_delay = int(input("Enter the amount of delay(in seconds) you want before positioning your mouse on the END position of the mouse drag:- "))
+                XendPositionForDrag = pyautogui.position().x
+                YendPositionForDrag = pyautogui.position().y
+                for i in range(1, input_delay+1):
+                    print("tik tik", i)
+                    time.sleep(1)
+                listForMouseDrag = ["MouseDrag", [XstartPositionForDrag, YstartPositionForDrag], [XendPositionForDrag, YendPositionForDrag]]
+                listOfActions.append(listForMouseDrag)
+                continue
+            elif newMacroActionType_UserInput == 4: #For Keypress
+                print("Press the key you want to be pressed with the macro(press escape after pressing the key)")
+                time.sleep(1)
+                actionKey = keyboard.record(until='esc')#its the key the user wants the program to press after he presses the macro key
+                actionKey = str(actionKey[0])
+                print(actionKey)
+                actionKey = actionKey[14:-6]
+                print("The key you pressed was --> ", actionKey)
+                listForKeypresses = ["Keypress", actionKey]
+                listOfActions.append(listForKeypresses)
+                
+            elif newMacroActionType_UserInput == 5: #for text
+                actionText = input("Enter the text you want to be typed with the macro(press escape after pressing the key)\n--> ")
+                listForText = ["Text", actionText]
+                listOfActions.append(listForText)
+                continue
+            elif newMacroActionType_UserInput == 6: #for Key Combination
+                actionKeyCombination = input("Enter the key combination/shortcut you want the macro to perform(Eg: ctrl,shift,alt,v)")
+                actionKeyCombination = actionKeyCombination.split(',')
+                listForKeyCombination = ["KeyCombination", actionKeyCombination]
+                listOfActions.append(listForKeyCombination)
+                continue                 
+            elif newMacroActionType_UserInput == 7: #for terminal commands
+                terminalCommand = input("Enter terminal command that you want to execute: ")
+                listForKeyCombination = ["TerminalCommand", terminalCommand]
+                listOfActions.append(listForKeyCombination)
+                continue
+            else:#for an invalid input
+                print("\nInvalid input try again!\n")
+                userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice: \n0. Exit\n1. Create new macros\n--> "))
+            pickle.dump(listOfActions, open("listOfAction.dat", "wb"))
+        elif userChoiceToMakeNewMacrosOrUseExisting == 2: # Use existing macros
+            return 0
+        userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter your choice:\n0. Exit\n1. Add more actions to this macro key"))
--- knut.py
+++ knut.py
@@ -8,7 +8,7 @@ from create_new_macro import createNewMacro
 listOfActions = []
 
 try:
-    inputMacroKey = pickle.load(open("inputMacroKeyStoringFile.dat", "rb"))
+    inputMacroKey = pickle.load(open("inputMacroKey.dat", "rb"))
 except:
     print("You have no saved shortkeys!")
     makeANewMacroOrNo = input("Make a new macro (y/n)").lower()
@@ -18,44 +18,54 @@ except:
         exit_code = createNewMacro()
         if exit_code == -1:
             exit()
-print("waiting for the macro key to be pressed...")
-keyboard.wait(inputMacroKey)
-listOfActions = pickle.load(open("listOfActionStoringFile.dat", "rb"))
-for i in listOfActions:
-    if i[0] == "MouseSingleClick":
-        xCoordinateOfOriginalMousePosition = pyautogui.position().x
-        yCoordinateOfOriginalMousePosition = pyautogui.position().y
-        pyautogui.click(i[1], i[2])
-        pyautogui.moveTo(xCoordinateOfOriginalMousePosition, yCoordinateOfOriginalMousePosition)
-    elif i[0] == "MouseDoubleClick":
-        xCoordinateOfOriginalMousePosition = pyautogui.position().x
-        yCoordinateOfOriginalMousePosition = pyautogui.position().y
-        pyautogui.doubleClick(i[1], i[2])
-        pyautogui.moveTo(xCoordinateOfOriginalMousePosition, yCoordinateOfOriginalMousePosition)
-    elif i[0] == "MouseDrag":
-        pyautogui.moveTo(i[1][0], i[1][1])
-        pyautogui.dragTo(i[2][0], i[2][1], button="left")
-    elif i[0] == "Keypress":
-        keyboard.press(i[1])
-    elif i[0] == "Text":
-        pyautogui.write(i[1])
-    elif i[0] == "KeyCombination":
-        if len(i[1]) == 2:
-            pyautogui.hotkey(i[1][0], i[1][1])
-        elif len(i[1]) == 3:
-            pyautogui.hotkey(i[1][0], i[1][1], i[1][2])
-        elif len(i[1]) == 4:
-            pyautogui.hotkey(i[1][0], i[1][1], i[1][2], i[1][3])
-        else:
-            print("Are you sure you added the correct key combination? This feature only works with a combination containing [2-4] keys.")
-    elif i[0] == "TerminalCommand":
-        try:
-            result = subprocess.run(i[1], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
-            print("Output:", result.stdout)
-            # Print the error, if any
-            if result.stderr:
-                print("Error:", result.stderr)
-            # Print the return code
-            print("Return Code:", result.returncode)
-        except Exception as e:
-            print("An error occurred:", str(e))
+        elif exit_code == 0:
+            inputMacroKey = pickle.load(open("inputMacroKey.dat", "rb"))
+            print("hello from code 0")
+
+userChoiceToMakeNewMacrosOrUseExisting = int(input("Enter the number corresponding to the choice:\n0. Exit\n1. Use existing macros\n2. Create new macro\n--> "))
+if(userChoiceToMakeNewMacrosOrUseExisting == 0):
+    exit()
+elif userChoiceToMakeNewMacrosOrUseExisting == 1:
+    print("waiting for the macro key to be pressed...")
+    keyboard.wait(inputMacroKey)
+    listOfActions = pickle.load(open("listOfAction.dat", "rb"))
+    for i in listOfActions:
+        if i[0] == "MouseSingleClick":
+            xCoordinateOfOriginalMousePosition = pyautogui.position().x
+            yCoordinateOfOriginalMousePosition = pyautogui.position().y
+            pyautogui.click(i[1], i[2])
+            pyautogui.moveTo(xCoordinateOfOriginalMousePosition, yCoordinateOfOriginalMousePosition)
+        elif i[0] == "MouseDoubleClick":
+            xCoordinateOfOriginalMousePosition = pyautogui.position().x
+            yCoordinateOfOriginalMousePosition = pyautogui.position().y
+            pyautogui.doubleClick(i[1], i[2])
+            pyautogui.moveTo(xCoordinateOfOriginalMousePosition, yCoordinateOfOriginalMousePosition)
+        elif i[0] == "MouseDrag":
+            pyautogui.moveTo(i[1][0], i[1][1])
+            pyautogui.dragTo(i[2][0], i[2][1], button="left")
+        elif i[0] == "Keypress":
+            keyboard.press(i[1])
+        elif i[0] == "Text":
+            pyautogui.write(i[1])
+        elif i[0] == "KeyCombination":
+            if len(i[1]) == 2:
+                pyautogui.hotkey(i[1][0], i[1][1])
+            elif len(i[1]) == 3:
+                pyautogui.hotkey(i[1][0], i[1][1], i[1][2])
+            elif len(i[1]) == 4:
+                pyautogui.hotkey(i[1][0], i[1][1], i[1][2], i[1][3])
+            else:
+                print("Are you sure you added the correct key combination? This feature only works with a combination containing [2-4] keys.")
+        elif i[0] == "TerminalCommand":
+            try:
+                result = subprocess.run(i[1], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True, shell=True)
+                print("Output:", result.stdout)
+                # Print the error, if any
+                if result.stderr:
+                    print("Error:", result.stderr)
+                # Print the return code
+                print("Return Code:", result.returncode)
+            except Exception as e:
+                print("An error occurred:", str(e))
+elif userChoiceToMakeNewMacrosOrUseExisting == 2: # create new macro
+    createNewMacro()
